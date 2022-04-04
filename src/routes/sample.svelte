ed96878f5ea52f5591de1388379c050208d6b974
Emmanuel Raju <emmanuel@able.do>
2022-04-04 19:47:07 +0530

    wip form field

--- src/routes/sample.svelte
+++ src/routes/sample.svelte
@@ -1,8 +1,17 @@
 <script>
   import Button from "$lib/components/Button.svelte"
+  import FormField from "$lib/components/FormField.svelte"
 </script> 
 <div class="flex flex-col space-y-4 p-10 items-center bg-[#fafada]">
-  <!-- <Button mode="primary" btnText="Button Text" isDisabled={false}/> -->
+  Buttons
   <Button className="btn-primary" btnText="Button Text" isDisabled={false}/>
+  <Button className="btn-primary" btnText="Button Text" isDisabled={true}/>
   <Button className="btn-secondary" btnText="Button Text" isDisabled={false}/>
+  <Button className="btn-secondary" btnText="Button Text" isDisabled={true}/>
+</div>
+<div class="flex flex-col space-y-4 p-10 items-center">
+  Form Field
+  <FormField isDisabled={false} className=""/>
+  <FormField isDisabled={false} className="error"/>
+  <FormField isDisabled={true} className=""/>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,14 +4,22 @@ module.exports = {
     extend: {
       colors: {
         grey : {
+          200:'#FAFAFA',
+          300:'#F2F4FA',
           400: '#EBECF2',
           500: '#C2C6D5',
+          700: '#5B6179',
           800: '#39405C',
           900: '#2B2F3E',
         },
         blue:{
           100: '#F8F9FB',
-        }
+          400: '#B3C5F5',
+        },
+        red:{
+          100: '#FFFBFC',
+          400: '#E77EA4',
+        },
       },
     },
   },
